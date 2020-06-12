5222da1989521c4cff8b6fb53e4f4f33932657b7
shriram-mnit <shriram969460@gmail.com>
2020-06-12 17:53:36 +0000

    build run

---
+++ precache-manifest.eed64bf0f51c7d649cd38b5f58af1fce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b029b4946e13ea414281eebca17f9dc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4fc4f2bf4dec5dfc891",
+    "url": "/static/css/main.13c8a689.chunk.css"
+  },
+  {
+    "revision": "50aae477cb98bd0398b2",
+    "url": "/static/js/2.9e17275c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.9e17275c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4fc4f2bf4dec5dfc891",
+    "url": "/static/js/main.554f3b83.chunk.js"
+  },
+  {
+    "revision": "e47aeda3be08a0b7f0fe",
+    "url": "/static/js/runtime-main.8a4eed9d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
